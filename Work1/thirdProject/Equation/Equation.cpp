e08bd785581884874051526a0bbf46d809ee48f3
OrangeSpacer <maxaev.art@yandex.ru>
2022-02-28 10:31:13 +0300

    feat: add work1 third task

---
+++ Work1/thirdProject/Equation/Equation.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int b;
+	int c;
+	int x;
+	cout << "Input b: ";
+	cin >> b;
+	cout << "Iput c: ";
+	cin >> c;
+	if (b != 0) {
+		x = (-c) / b;
+		cout << "Answer: " << x << endl;
+	}
+	else {
+		cout << "Error" << endl;
+	}
+}
