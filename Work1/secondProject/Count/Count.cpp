479766cb49baaf8afcbd6705c2d68be427738e28
OrangeSpacer <maxaev.art@yandex.ru>
2022-02-28 10:30:13 +0300

    feat: add work1 second task

---
+++ Work1/secondProject/Count/Count.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+	cout << "Input a" << endl;
+	cin >> a;
+	cout << "Input b" << endl;
+	cin >> b;
+	cout << "Summ answer:" << a+b << "\n" << "Difference answer:" << a - b << "\n" << "Multiplication answer:" << a * b << "\n" << "Division answer:" << a / b << endl;
+	return 0;
+}
