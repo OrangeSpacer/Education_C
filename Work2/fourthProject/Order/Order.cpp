eb40b46034b99edf9195b39387ebb0193c540ad5
OrangeSpacer <maxaev.art@yandex.ru>
2022-02-28 10:37:16 +0300

    feat: add work2 fourth task

---
+++ Work2/fourthProject/Order/Order.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float N, i;
+    int N1;
+    cout << "Input N: ";
+    cin >> N;
+    if (N > 0) {
+        for (i = 1; i <= 10; i++) {
+            N = N + 1;
+            N1 = N;
+            cout << N1 << " ";
+        }
+    }
+    else {
+        cout << "Error";
+    }
+}
