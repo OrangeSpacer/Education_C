35481e640d36f8eeaf26cc34fb106c1904902a50
OrangeSpacer <maxaev.art@yandex.ru>
2022-02-28 10:46:14 +0300

    feat: add work2 task

---
+++ Work2/fifthProject/Tabulation/Tabulation.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float x, y;
+
+    for (x = -4; x <= 4; x = x + 0.5) {
+        if (x != 1) {
+            y = (x * x - (2 * x) + 2) / (x - 1);
+            cout << y << endl;
+        }
+        else {
+            continue;
+        }
+    }
+}
+
