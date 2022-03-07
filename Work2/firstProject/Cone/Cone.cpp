25558099983d2188e12b61fe65b1d0d4122fbd99
OrangeSpacer <maxaev.art@yandex.ru>
2022-03-07 11:10:16 +0300

    feat: add new condition

--- Work2/firstProject/Cone/Cone.cpp
+++ Work2/firstProject/Cone/Cone.cpp
@@ -9,10 +9,9 @@ int main(){
     cin >> h;
     cout << "Input R: ";
     cin >> R;
-    cout << "Input l: ";
-    cin >> l;
 
-    if ((h > 0) && (r > 0) && (R > 0) && (l > 0)) {
+    if ((h > 0) && (r > 0) && (R > 0)) {
+        l = sqrt(abs(R - r) * abs(R - r) + h * h);
         float V = (1.0 / 3.0) * 3.14 * h * (R * R + R * r + r * r);
         float S = 3.14 * (R * R + (R + r) * l + r * r);
         cout << "The volume of the cone is equal to: " << V << endl << "The total surface of the cone is equal to: " << S;
