f00ffdf7224310a502ec16fe818221f8e4ce30de
OrangeSpacer <maxaev.art@yandex.ru>
2022-03-14 23:15:31 +0300

    feat: add work3 first task

---
+++ work3/secondProject/Loan/Loan.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	float S, p, n, r, m;
+	cout << "Input S: ";
+	cin >> S;
+	cout << "Input p: ";
+	cin >> p;
+	cout << "Input n: ";
+	cin >> n;
+
+	r = p / 100;
+	
+	if ((pow((1 + r), n) - 1) != 0) {
+		m = (S * r * (pow((1 + r), n))) / (12 * (pow((1 + r), n)));
+		cout << "Answer m: " << m;
+	}
+}
+
