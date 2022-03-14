cf0549319f77bb7936e4b6b35af1fa78cede535b
OrangeSpacer <maxaev.art@yandex.ru>
2022-03-14 23:28:25 +0300

    feat: add work 3 second task

---
+++ work3/firstProject/Loan2/Loan2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double S, m, n, p, r, m1;
+    cout << "Input S: ";
+    cin >> S;
+    cout << "Input m: ";
+    cin >> m;
+    cout << "Input n: ";
+    cin >> n;
+    
+    m1 = 0;
+    for (p = 1; m1 < m; p++)
+    {
+        r = p / 100;
+        m1 = ((S * r * (pow((1 + r), n))) / (12 * (pow((1 + r), n)) - 1));
+        if (m1 > m) {
+            cout << "Answe p: " << p << "%" << endl;
+        }
+        else {
+            cout << "Error " << endl;
+        }
+    }
+}
