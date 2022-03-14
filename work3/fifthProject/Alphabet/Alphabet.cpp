a69a2e2aa15e6fc897426d5f21be48cac51ca620
OrangeSpacer <maxaev.art@yandex.ru>
2022-03-15 00:12:15 +0300

    feat: add work3 fifth task

---
+++ work3/fifthProject/Alphabet/Alphabet.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char alphabet[30];
+    
+    
+    cout << "Input word: ";
+    cin >> alphabet;
+
+    for (int i = 0; i < strlen(alphabet) - 1; i++)
+    {
+        for (int j = i; j < strlen(alphabet) - 1; j++)
+        {
+            if (alphabet[i] > alphabet[j + 1])
+            {
+                swap(alphabet[i], alphabet[j + 1]);
+            }
+        }
+    }
+    cout << alphabet;
+}
