79fd335c018cbf4bf4fb1058045ac51a4fdae573
OrangeSpacer <maxaev.art@yandex.ru>
2022-03-15 00:05:43 +0300

    feat: add work3 fourth task

---
+++ work3/fourthProject/Filter/Filter.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string path = "taskFile.txt";
+    ofstream fout;
+
+    int Number;
+
+    fout.open(path);
+
+
+    if (!fout.is_open()) {
+        cout << "Error";
+    }
+
+    else {
+        fout << 5545454 << "frgreg" << 666 << endl;
+    }
+
+    ifstream file(path);
+
+
+    do {
+        if (file >> Number) {
+            cout << Number << endl;
+        }
+        else {
+            file.clear();
+            file.ignore(1, ' ');
+        }
+    } 
+    while (!file.eof());
+    fout.close();
+}
