f36de4a30495c6b6d56471871a85f91cfd00a6fb
OrangeSpacer <maxaev.art@yandex.ru>
2022-03-14 23:47:45 +0300

    feat: add work3 third task

---
+++ work3/thirdProject/CopyingFile/CopyingFile.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    string path = "taskFile.txt";
+    ofstream fout;
+
+    fout.open(path);
+
+
+    if (!fout.is_open()) {
+        cout << "Error";
+    }
+    
+    else {
+        fout << "Data";
+    }
+
+    fout.close();
+}
+
