1f08b22583278b76bca0cab2ce0f85dc6053bacf
OrangeSpacer <maxaev.art@yandex.ru>
2022-04-25 17:07:10 +0300

    feat: add secondProject

---
+++ work4/secondProject/file/file.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+//int main()
+//{
+//    int count = 0;
+//    string path = "taskFile.txt";
+//    ofstream fout;
+//
+//    fout.open(path);
+//
+//    fout << 1345678932;
+//
+//    fout.close();
+//
+//    fout.open(path);
+//
+//    
+//    if(fout.is_open()) {
+//
+//        for (int i = 0; i < strlen(fout.data());i++) {
+//            count += fout[i];
+//        }
+//    }
+//    cout << count;
+//}
+
+
+//Задание находится в разработке!!!!!!!!!!!
