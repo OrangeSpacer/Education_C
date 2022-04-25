33af46097ad04a9a30678b21382d0a54f9e282bf
OrangeSpacer <maxaev.art@yandex.ru>
2022-04-25 17:29:15 +0300

    feat: add firstProject

---
+++ work4/firstProject/The sign of the number/The sign of the number.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void func(int n) {  // функция
+	if (n != 0) {
+		cout << (n > 0 ? 1 : -1);
+	}
+	else {
+		cout << 0;
+	}
+}
+
+int main() {
+	int number;
+	cout << "Input number: ";
+	cin >> number;
+	func(number);  // вызов функции
+}
