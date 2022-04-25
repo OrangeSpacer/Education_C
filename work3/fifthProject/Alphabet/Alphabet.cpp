a72c7bd9b2827c8e57d7a6f8ab0bea7f0a6c93dc
OrangeSpacer <maxaev.art@yandex.ru>
2022-04-25 16:30:17 +0300

    cash add

--- work3/fifthProject/Alphabet/Alphabet.cpp
+++ work3/fifthProject/Alphabet/Alphabet.cpp
@@ -9,10 +9,9 @@ int main()
     cout << "Input word: ";
     cin >> alphabet;
 
-    for (int i = 0; i < strlen(alphabet) - 1; i++)
-    {
-        for (int j = i; j < strlen(alphabet) - 1; j++)
-        {
+    for (int i = 0; i < strlen(alphabet) - 1; i++){
+        for (int j = i; j < strlen(alphabet) - 1; j++){
+            cout << alphabet[i] << ":" << alphabet[j] << endl;
             if (alphabet[i] > alphabet[j + 1])
             {
                 swap(alphabet[i], alphabet[j + 1]);
