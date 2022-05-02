8bd3380424b53374cd372394b5cd64278e464a67
OrangeSpacer <maxaev.art@yandex.ru>
2022-05-02 18:05:46 +0300

    refactor: change seventyProject

---
+++ work4/seventhProject/generator/generator.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    int s0, m, b, c, s1, count;
+
+    cout << "Start number:";
+    cin >> s0;
+    cout << "Input m: ";
+    cin >> m;
+    cout << "Input b: ";
+    cin >> b;
+    cout << "Input c: ";
+    cin >> c;
+    cout << "End number s1: ";
+    cin >> s1;
+
+    for (int i = 0; i < s1; i++) {
+        s0 = ((m * s0 + b)) % c;
+        cout << s0 << endl;
+    }
+}
